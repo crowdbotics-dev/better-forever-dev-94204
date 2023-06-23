e995f5ff522e8758c21cb18860bb94614ae4f04b
Crowdbotics <engineering@crowdbotics.com>
2023-06-23 13:23:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "better-forever-dev-94204",
+  url: "https://better-forever-dev-94204.botics.co",
+  api: "https://better-forever-dev-94204.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
